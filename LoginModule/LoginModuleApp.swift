f6ecd6e206e3ae09872dfb97d8cc19f69fe166d1
islamlotfy <islam.lotfy@nagwa.com>
2021-12-01 09:53:36 +0200

    Initial Commit

---
+++ LoginModule/LoginModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginModuleApp.swift
+//  LoginModule
+//
+//  Created by Lotfy on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
